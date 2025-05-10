f33aa5aa6edabd1fe3902e395615246c55184197
PERSONAL_BORIS <borisisac@gmail.com>
2025-05-10 16:31:31 +0100

    Exersize_1

---
+++ main.py
@@ -0,0 +1,10 @@
+s1 = "Hello World"
+s2 = "   fly me   to   the moon  "
+s3 = "luffy is still joyboy"
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        # Split the string into words
+        word = s1
+        
